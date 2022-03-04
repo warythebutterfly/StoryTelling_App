b99f9811abc7c0125ff69a74c159ea6ca48dc8f5
temit <temitoyosi@gmail.com>
2022-03-04 12:51:51 +0100

    added response message for contact form

--- custom/custom.js
+++ custom/custom.js
@@ -12,7 +12,7 @@ $('#loginForm').submit(function (e) {
 
 
     console.log(data); 
-    if(data.email != "" || data.password != ""){
+    if(data.email != "" && data.password != ""){
         signIn();
     }
     else{
@@ -38,7 +38,7 @@ $('#signUpForm').submit(function (e) {
 
 
     console.log(data); 
-    if(data.email != "" || data.password != "" || data.firstname != "" || data.lastname != "" || data.confirmPassword != ""){
+    if(data.email != "" && data.password != "" && data.firstname != "" && data.lastname != "" && data.confirmPassword != ""){
        if(data.password == data.confirmPassword) signUp();
        else swal("Not yet", "Passwords do not match", "warning");
     }
@@ -76,32 +76,32 @@ $('#signUpForm').submit(function (e) {
 //     }
 // });
 
-// $('#editStoryForm').submit(function (e) {
+$('#contactForm').submit(function (e) {
     
-//     e.preventDefault();
-//     //console.log("before contact form");
+    e.preventDefault();
+    //console.log("before contact form");
     
-//     var data = {
-//         title:$('#storytitle').val(),
-//         storyLocation:$('#storylocation').val(),
-//         locationImage: $('#img').val(),
-//         storyType: $('#storytype').val(),
-//         body:$('#storybody').val(),
+    var data = {
+        // firstname:$('#storytitle').val(),
+        name:$('#name').val(),
+        email: $('#email').val(),
+        subject: $('#subject').val(),
+        body:$('#message').val(),
         
-//     }
+    }
 
 
-//     console.log(data); 
-//     if(data.title != "" || data.storyLocation != "" || data.locationImage != "" || data.storyType != "" || data.body != ""){
-//        editStory();
+    console.log(data); 
+    if(data.name != "" && data.email != "" && data.subject != "" && data.body != ""){
+       contact();
        
-//     }
-//     else{
+    }
+    else{
 
-//       swal("Not yet", "All fields are required!", "warning");
+      swal("Not yet", "All fields are required!", "warning");
 
-//     }
-// });
+    }
+});
 
 function signIn(){
 
@@ -203,6 +203,32 @@ function editStory(){
       swal("Updated Successfully", "Thank you for sharing your unique voice and original story", "success");
       $("#btnedit").removeAttr("disabled");
                 $("#btnedit").html("Save changes");
+    console.log("i got here;");
+    //window.setTimeout(window.location.assign("st_index.html"), 10000);
+    }, 3000);
+};
+
+function contact(){
+
+    $("#btncontact").html("<i class = 'fa fa-spinner fa-spin'></i> Please Wait").css('margin', '10px');
+    $("#btncontact").attr("disabled", "disabled");
+    window.setTimeout(function() {
+     
+      // swal({
+      //   title:"Login Succeessful",
+      //   type: "success",
+      //   showCancelButton:true
+      // },
+      // function(isConfirm){
+      //   debugger;
+      //   setTimeout(function(){
+
+      //   })
+      // }
+      // )
+      swal("Sent Successfully", "Thank you for contacting us. We would get back to you shortly", "success");
+      $("#btncontact").removeAttr("disabled");
+                $("#btncontact").html("Save Message");
     console.log("i got here;");
     //window.setTimeout(window.location.assign("st_index.html"), 10000);
     }, 3000);
